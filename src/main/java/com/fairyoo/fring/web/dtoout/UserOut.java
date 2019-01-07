73da05251b6532f0b7eea6119f5a9c975aba02f7
Fairy <fairyoo@qq.com>
2019-01-08 00:19:23 +0800

    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", locale = "zh", timezone = "GMT+8")

--- src/main/java/com/fairyoo/fring/web/dtoout/UserOut.java
+++ src/main/java/com/fairyoo/fring/web/dtoout/UserOut.java
@@ -65,7 +65,7 @@ public class UserOut implements Serializable, IOut {
      * @author by MengYi at 2019-01-02 20:12
      */
     @ApiModelProperty(value = "创建时间")
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", locale = "zh", timezone = "GMT+8")
     private Date creationTime;
 
 
@@ -75,7 +75,7 @@ public class UserOut implements Serializable, IOut {
      * @author by MengYi at 2019-01-02 20:12
      */
     @ApiModelProperty(value = "修改时间")
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", locale = "zh", timezone = "GMT+8")
     private Date lastModificationTime;
 
     public UserOut() {

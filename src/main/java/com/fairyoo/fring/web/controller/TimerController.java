1b2ec9bd81fdddfd9dbdce0e7944cd13611de60a
Fairy <fairyoo@qq.com>
2019-01-08 10:06:54 +0800

    spring.jpa.database-platform=org.hibernate.dialect.MySQLDialect

---
+++ src/main/java/com/fairyoo/fring/web/controller/TimerController.java
@@ -0,0 +1,47 @@
+package com.fairyoo.fring.web.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.var;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * SpringBoot几种定时任务的实现方式
+ *
+ * Timer：这是java自带的java.util.Timer类，这个类允许你调度一个java.util.TimerTask任务。使用这种方式可以让你的程序按照某一个频度执行，但不能在指定时间运行。一般用的较少。
+ * ScheduledExecutorService：也jdk自带的一个类；是基于线程池设计的定时任务类,每个调度任务都会分配到线程池中的一个线程去执行,也就是说,任务是并发执行,互不影响。
+ * Spring Task：Spring3.0以后自带的task，可以将它看成一个轻量级的Quartz，而且使用起来比Quartz简单许多。
+ * Quartz：这是一个功能比较强大的的调度器，可以让你的程序在指定时间执行，也可以按照某一个频度执行，配置起来稍显复杂。
+ *
+ *
+ * @author MengYi at 2019-01-08 09:26
+ */
+@Api(tags = "定时任务", value = "")
+@RestController
+@RequestMapping(value = "/fring/timer", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+public class TimerController {
+
+
+    @ApiOperation(value = "TimerTask")
+    @GetMapping(value = "/timerTask")
+    public void timerRun() {
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println("task  run:"+ new Date());
+            }
+        };
+
+        var t = new Timer();
+        t.schedule(timerTask, 10,3000);
+
+    }
+}
